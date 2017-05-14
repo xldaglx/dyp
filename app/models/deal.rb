d77730545f26966c39dcb5889edcf35825d41292
dag <gerardoayalad@outlook.com>
2017-05-13 20:14:23 -0500

    Second Update Mayo 13

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -1,8 +1,8 @@
 class Deal < ApplicationRecord
   belongs_to :user
   has_many :categories
-  has_many :behaviors
-  has_many :comments
+  has_many :behaviors, :dependent => :destroy
+  has_many :comments, :dependent => :destroy
   has_attached_file :promoimage, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :promoimage, content_type: /\Aimage\/.*\z/
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
   get 'image/scrapp', to: 'deals#scrapp'
   get 'updateRank', to: 'deals#rank'
   get 'moderate', to: 'deals#moderate'
+  get "myprofile/:id" => "users#profile"
+
   devise_for :users, :controllers => { sessions: 'users/sessions' ,:omniauth_callbacks => "users/omniauth_callbacks",  registrations: 'users/registrations' }
 end
