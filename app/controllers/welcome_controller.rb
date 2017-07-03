69882e889ea5d9d25daaa6151e1b14bd16284835
Eduardo Aguilar <rockyturbo@gmail.com>
2017-07-03 11:02:44 -0500

    css

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    @deals = Deal.all.order('created_at DESC').page(params[:page])
+    @deals = Deal.all.order('created_at DESC').page(params[:page]).per(3)
     @hot_deal = Deal.find(10)
     @top_voted = Deal.all
   end
