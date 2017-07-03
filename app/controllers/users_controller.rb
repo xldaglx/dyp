aec8eeae671e2dfcbb860e65d8a4c948b452bffa
dag <gerardoayalad@outlook.com>
2017-07-03 14:47:18 -0500

    commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,13 @@ class UsersController < ApplicationController
 	end
 
 	def deals
-    	@deals = Deal.where('user_id',params[:id])  
+    	@deals = Deal.where('user_id ='+params[:id])
+		@promoHot = User.joins(:deals).where('deals.rank > 1').where('deals.user_id ='+current_user.id.to_s).count
 	end
 
 	def favorites
     	@deals = Deal.joins('LEFT JOIN favorites ON deals.id = favorites.deal_id').where('favorites.user_id = '+current_user.id.to_s)
+		@promoHot = User.joins(:deals).where('deals.rank > 1').where('deals.user_id ='+current_user.id.to_s).count
 	end
 
 	def ranking
@@ -18,6 +20,13 @@ class UsersController < ApplicationController
 
 	def rated
 		@deals = Deal.joins('LEFT JOIN behaviors ON deals.id = behaviors.deal_id').where('behaviors.user_id = '+current_user.id.to_s)
+		@promoHot = User.joins(:deals).where('deals.rank > 1').where('deals.user_id ='+current_user.id.to_s).count
+	end
+	
+	def publicprofile
+    	@deals = Deal.where('user_id = '+params[:id])
+    	@user = User.find(params[:id])
+		@promoHot = User.joins(:deals).where('deals.rank > 1').where('deals.user_id ='+params[:id].to_s).count
 	end
 
 end
