745606ff60fa3e848659e1298688a31f4bc9b914
dag <gerardoayalad@outlook.com>
2017-07-03 12:48:05 -0500

    Perfil Publico

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
 	def profile
 		@account = User.find(current_user)
-		@promoHot = User.joins(:deals).where('deals.rank > 1').where('deals.user_id', current_user.id).count
+		@promoHot = User.joins(:deals).where('deals.rank > 1').where('deals.user_id ='+current_user.id.to_s).count
 	end
 
 	def deals
-    	@deals = Deal.where('user_id',params[:id])  
+    	@deals = Deal.where('user_id ='+params[:id])
 	end
 
 	def favorites
@@ -18,6 +18,11 @@ class UsersController < ApplicationController
 
 	def rated
 		@deals = Deal.joins('LEFT JOIN behaviors ON deals.id = behaviors.deal_id').where('behaviors.user_id = '+current_user.id.to_s)
+	end
+
+	def publicprofile
+    	@deals = Deal.where('user_id = '+params[:id])
+    	@user = User.find(params[:id])
 	end
 
 end
