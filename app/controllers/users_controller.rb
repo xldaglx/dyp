e821d2b7101be6c1fed8425efab85526dbd4ecb6
dag <gerardoayalad@outlook.com>
2017-07-03 10:35:11 -0500

    Votadas Menu

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,10 @@ class UsersController < ApplicationController
     	@deals = Deal.joins('LEFT JOIN favorites ON deals.id = favorites.deal_id').where('favorites.user_id = '+current_user.id.to_s)
 	end
 
+	def rated
+		@deals = Deal.joins('LEFT JOIN behaviors ON deals.id = behaviors.deal_id').where('behaviors.user_id = '+current_user.id.to_s)
+	end
+
 	def require_permission
 		if current_user.id != User.find(params[:id]).id
 		redirect_to root_path
