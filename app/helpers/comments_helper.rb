918bf5c6eeedc8ad00496fd1f957a6da17a77f1e
Eduardo Aguilar <rockyturbo@gmail.com>
2017-07-11 16:48:51 -0500

    Edición de comentarios
    Cambié el diseño pero necesito que cuando le des clic a me gusta
    automáticamente cambie el número de la izquierda, porque ahorita debes
    darle refresh para ver reflejados la suma de likes.

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -3,21 +3,21 @@ module CommentsHelper
 		if user_signed_in?
 			like = Like.where('user_id='+current_user.id.to_s).where('comment_id='+commentid.to_s).first
 			if like.present?
-				html = '<div class="like liked" likeid="'+like.id.to_s+'" commentid="'+commentid.to_s+'" de="'+current_user.id.to_s+'" >Quitale el Like</div>'
+				html = '<div class="like liked rojo" likeid="'+like.id.to_s+'" commentid="'+commentid.to_s+'" de="'+current_user.id.to_s+'" >Ya no me gusta</div>'
 				return html.html_safe
 			else
-				html = '<div class="like" commentid="'+commentid.to_s+'" de="'+current_user.id.to_s+'" >Dale Like</div>'
+				html = '<div class="like" commentid="'+commentid.to_s+'" de="'+current_user.id.to_s+'" >Me gusta</div>'
 				return html.html_safe
 			end
 		else
-			html = "<div class=''>Dale Like y muestra modal</div>"
+			html = "<div class='like2'>Me gusta</div>"
 			return html.html_safe
 		end
 	end
 	def getParent(commentid)
 		@comment = Comment.find(commentid)
 		if @comment.present?
-			html = '<div class="parent_comment">Citando ->:'+@comment.description+'</div>'
+			html = '<div class="parent_comment"><span class="glyphicon glyphicon-pencil"></span> Respuesta a:'+@comment.description+'</div>'
 			return html.html_safe
 		else
 			html = 'No extra comments'
