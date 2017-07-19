798a833978cd4e932726b490dcdd6526ac211c7f
Eduardo Aguilar <rockyturbo@gmail.com>
2017-07-19 16:46:41 -0500

    Cambios de Asana
    Junto con otros detallados de texto e imágenes, falta alinear el modal
    de Ver Comportamiento

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,7 +4,7 @@ module UsersHelper
 		if @relationship.present?
 			button = ""
 			@relationship.each do |relationship|
-				button = '<a class="btn-danger unfollow" href="#" idf="'+relationship.id.to_s+'"><i class="fa fa-delete" aria-hidden="true"></i> Dejar de seguir</a>'
+				button = '<a class="btn-noseguir unfollow" href="#" idf="'+relationship.id.to_s+'"><i class="fa fa-bell-slash-o" aria-hidden="true"></i> Dejar de seguir</a>'
 			end			
 		else
 			button = '<a class="btn-seguir follow" href="#" user_followed="'+followed.to_s+'"><i class="fa fa-bell-o" aria-hidden="true"></i> Seguir</a>'
