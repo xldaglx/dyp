00b900a7aae1b418998b8f9e21845a16b844f71f
Eduardo Aguilar <rockyturbo@gmail.com>
2017-07-20 13:20:32 -0500

    Más Detalles
    Cambios en los mails, y en las alertas, también en cada interior de los
    de moderar.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,7 @@
 class WelcomeController < ApplicationController
   def index
-  	@hotvalue = Setting.find_by(core: 'hot_deal');
-  	@topvalue = Setting.find_by(core: 'top_voted');
-  	p Deal.find(@topvalue.value.split(','))
+  	@hotvalue = Setting.find_by(core: 'hot_deal')
+  	@topvalue = Setting.find_by(core: 'top_voted')
   	@hotvalue.present? ? @hot_deal = Deal.find(@hotvalue.value.to_i) : @hot_deal = Deal.where('status = 1').order('rank DESC').first
   	@topvalue.present? ? @top_voted = Deal.find(@topvalue.value.split(',')) : @top_voted = Deal.where('status = 1').order('rank DESC')
     @deals = Deal.all.where('status = 1').order('created_at DESC').page(params[:page])
