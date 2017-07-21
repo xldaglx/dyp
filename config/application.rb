07149ee0901aea9715af459e382b7ed6e544172c
dag <gerardoayalad@outlook.com>
2017-07-21 10:07:01 -0500

    Images Fixed

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Promodescuentos
+module Descuentosypromociones
   class Application < Rails::Application
   config.time_zone = 'Mexico City'
     # Settings in config/environments/* take precedence over those specified here.
