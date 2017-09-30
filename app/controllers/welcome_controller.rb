566c697bc3da450d73ebcf0ab174acc53e32d15a
dag <gerardoayalad@outlook.com>
2017-09-30 18:20:58 -0500

    Deals performance

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,8 +3,8 @@ class WelcomeController < ApplicationController
   	@hotvalue = Setting.find_by(core: 'hot_deal')
   	@topvalue = Setting.find_by(core: 'top_voted')
   	@hotvalue.present? ? @hot_deal = Deal.find(@hotvalue.value.to_i) : @hot_deal = Deal.where('status = 1').order('rank DESC').first
-  	@topvalue.present? ? @top_voted = Deal.find(@topvalue.value.split(',')) : @top_voted = Deal.where('status = 1').order('rank DESC')
-    @deals = Deal.all.where('status = 1').order('created_at DESC').page(params[:page]).per(33)
+  	@topvalue.present? ? @top_voted = Deal.find(@topvalue.value.split(',')) : @top_voted = Deal.where('status = 1').order('rank DESC').limit(3)
+    @deals = Deal.all.where('status = 1').order('created_at DESC').page(params[:page]).per(24)
   end
   def ayuda
   	@banners = Banner.order("RAND()").limit(3)
